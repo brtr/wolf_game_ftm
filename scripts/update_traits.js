97ced8ac77eb7b259458e3a5796c7cbc3dcffd0c
Ian <xiongbo027@gmail.com>
2021-12-20 03:27:05 +0000

    update traits by adding delay

--- scripts/update_traits.js
+++ scripts/update_traits.js
@@ -1,4 +1,5 @@
 const traits = require("../data.json");
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function main() {
   const NFT = await hre.ethers.getContractFactory("Traits");
@@ -8,6 +9,7 @@ async function main() {
   for (const trait of traits) {
     const traitIds = [...Array(trait.data.length).keys()];
     await contract.uploadTraits(trait.id, traitIds, trait.data);
+    await delay(10000);
   }  // add data to traits
 }
 
